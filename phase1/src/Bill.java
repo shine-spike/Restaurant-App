ef0dbee7c5766c582211abec65ebf6d962347441
thomas.passaro@mail.utoronto.ca <thomas.passaro@mail.utoronto.ca>
2018-03-01 14:34:30 -0500

    Add variables and method headers for Bill

--- phase1/src/Bill.java
+++ phase1/src/Bill.java
@@ -1,2 +1,21 @@
-public class Bill {
+import java.util.ArrayList;
+
+public class Bill
+{
+  private ArrayList<Order> orders;
+
+  public Bill()
+  {
+    orders = new ArrayList<>();
+  }
+
+  public void addOrder(Order order)
+  {
+      orders.add(order);
+  }
+
+  public String toString()
+  {
+      return "";
+  }
 }
