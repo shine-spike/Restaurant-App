b3363046224d8778f3548ecf14a96af02af83835
Matthew Verreault <matthew.verreault@mail.utoronto.ca>
2018-03-01 13:57:58 -0500

    Implemented Order methods

--- phase1/src/Order.java
+++ phase1/src/Order.java
@@ -1,30 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Order {
-    static private int current_order_number = 0;
+  private static int current_order_number = 0;
 
-    private int tableNum;
-    private int serverId;
-    private MenuItem menuItem;
-    private Ingredient[] additions;
-    private Ingredient[] subtractions;
-    private boolean seen;
+  private int tableNum;
+  private int serverId;
+  private MenuItem menuItem;
+  private Ingredient[] additions;
+  private Ingredient[] subtractions;
+  private boolean seen;
 
-    public int getTableNum() {
-        return tableNum;
-    }
+  public int getTableNum() {
+    return tableNum;
+  }
 
-    public MenuItem getMenuItem() {
-        return menuItem;
-    }
+  public MenuItem getMenuItem() {
+    return menuItem;
+  }
 
-    public int getPrice(){
-        return menuItem.getPrice();
-    }
+  public int getPrice() {
+    return menuItem.getPrice();
+  }
 
-    public String toString(){
-        menuItem.toString();
-    }
+  public String toString() {
+    // TODO May need to add information depending on menuItem.toString()
+    // TODO Need to add information about additions, and subtractions
+    return menuItem.toString();
+  }
 
-    public Ingredient[] getIngredientsList(){
-        return null;
-    }
+  public ArrayList<Ingredient> getIngredientsList() {
+    Ingredients[] ingredients = menuItem.getIngredients();
+    ArrayList[] ingredientsList = new ArrayList<Ingredients>(Arrays.asList(ingredients));
+
+    for(Ingredient i : subtractions){ingredientList.remove(i);}
+    for(Ingredient i : additions){ingredientList.add(i);}
+
+    return ingredientsList;
+  }
 }
