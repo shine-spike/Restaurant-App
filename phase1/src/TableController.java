bd6502b0e1dd8afb06eb67108e23e2fc796ff25a
Ziyad Edher <ziyad.edher@gmail.com>
2018-03-01 16:05:27 -0500

    Fix formatting

--- phase1/src/TableController.java
+++ phase1/src/TableController.java
@@ -1,29 +1,23 @@
-public class TableController
-{
+public class TableController {
   private Table[] tableList;
 
-  public TableController(int numTables)
-  {
-      tableList = new Table[numTables];
-      this.createTables();
+  public TableController(int numTables) {
+    tableList = new Table[numTables];
+    this.createTables();
   }
 
-  private void createTables()
-  {
-    for (int i = 1; i <= tableList.length; i++)
-    {
+  private void createTables() {
+    for (int i = 0; i < tableList.length; i++) {
       tableList[i] = new Table(i);
     }
   }
 
-  public void addToBill(int tableNumber, Order order)
-  {
+  public void addToBill(int tableNumber, Order order) {
     Table myTable = tableList[tableNumber - 1];
     myTable.addToBill(order);
   }
 
-  public String printBill(int tableNumber)
-  {
+  public String printBill(int tableNumber) {
     return tableList[tableNumber - 1].printBill();
   }
 
