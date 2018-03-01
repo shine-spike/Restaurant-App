daa0e93aed5fd5fda1a89247b75ef4af48acc9bc
thomas.passaro@mail.utoronto.ca <thomas.passaro@mail.utoronto.ca>
2018-03-01 15:13:30 -0500

    Write Bill.toString

--- phase1/src/Bill.java
+++ phase1/src/Bill.java
@@ -11,11 +11,28 @@ public class Bill
 
   public void addOrder(Order order)
   {
-      orders.add(order);
+    orders.add(order);
+  }
+
+  public int billTotal()
+  {
+    int total = 0;
+    for (Order order : orders)
+    {
+      total += order.getPrice();
+    }
+    return total;
   }
 
   public String toString()
   {
-      return "";
+    StringBuilder str = new StringBuilder();
+    str.append("Bill: \n");
+    for (Order order: orders)
+    {
+      str.append(order.toString()).append("\n");
+    }
+    str.append("Bill total: \n");
+    return str.toString();
   }
 }
