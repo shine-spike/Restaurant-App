330e9240e35ea888a549bb5e14a1ec4a1f7716c8
Matthew Verreault <matthew.verreault@mail.utoronto.ca>
2018-03-01 16:32:39 -0500

    Added the enum EventType

---
+++ phase1/src/EventType.java
@@ -0,0 +1,34 @@
+public enum EventType {
+    ORD,
+    SEEN,
+    READY,
+    REC,
+    CON,
+    RET,
+    REDO,
+    BILL,
+    PAID;
+
+    public void process(){
+        switch(this){
+            case ORD:
+                break;
+            case SEEN:
+                break;
+            case READY:
+                break;
+            case REC:
+                break;
+            case CON:
+                break;
+            case RET:
+                break;
+            case REDO:
+                break;
+            case BILL:
+                break;
+            case PAID:
+                break;
+        }
+    }
+}
