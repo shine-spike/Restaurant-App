f6f942e4c8c5f51f728ec880e0af7d03bb5fa2d7
Ziyad Edher <ziyad.edher@mail.utoronto.ca>
2018-03-05 12:52:05 -0500

    Refactor Parser to improve structure and add documentation

--- phase1/src/Parser.java
+++ phase1/src/Parser.java
@@ -15,13 +15,15 @@ public class Parser {
    */
   private static final char COMMENT_SYMBOL = '%';
 
+  // Where to read the files from
+  private String filesLocation = "phase1/config/";
+
   // The names of all the files we need to access.
   private static final String EMPLOYEE_FILE_NAME = "employees.txt";
   private static final String INGREDIENTS_FILE_NAME = "ingredients.txt";
   private static final String MENUS_FILE_NAME = "menus.txt";
   private static final String EVENTS_FILE_NAME = "events.txt";
 
-  private String filesLocation = "phase1/config/";
   private Restaurant restaurant;
 
 
@@ -44,65 +46,40 @@ public class Parser {
   }
 
   /**
+   * Verifies that the given configuration folder exists
+   * and creates it on the spot with a notification if not.
+   */
+  private void verifyConfigurationFolder() {
+    // Make sure that the configuration file location exists and
+    // notify if it was created right now
+    Boolean madeNew = new File(filesLocation).mkdirs();
+    if (madeNew) {
+      System.out.println("Configuration folder not found. Created a new one.");
+    }
+  }
+
+  /**
    * Parses all the configuration files needed.
    */
   public void parseConfiguration() {
-    // Make sure that the configuration file location exists and
-    // notify if it was created right now
-    Boolean madeNew = new File(filesLocation).mkdir();
-    if (madeNew) {
-      System.out.println("Created a configuration folder.");
-    }
+    // Make sure the configuration folder exists
+    verifyConfigurationFolder();
 
-    // Generate the readers for each type of configuration file.
-    BufferedReader employeeReader;
-    BufferedReader ingredientReader;
-    BufferedReader menuReader;
-
-    try {
-      employeeReader = getConfigurationReader(EMPLOYEE_FILE_NAME);
-      ingredientReader = getConfigurationReader(INGREDIENTS_FILE_NAME);
-      menuReader = getConfigurationReader(MENUS_FILE_NAME);
-    } catch (IOException e) {
-      // TODO: be more delicate
-      System.out.println("Finding configuration files failed.");
-      System.exit(1);
-      return;
-    }
-
-    // Parse each type of configuration file and the events file
-    try {
-      parseEmployeeConfiguration(employeeReader);
-      parseIngredientConfiguration(ingredientReader);
-      parseMenuConfiguration(menuReader);
-    } catch (IOException e) {
-      // TODO: be more delicate
-      System.out.println("Parsing of configuration files was not successful.");
-      System.exit(1);
-    }
+    // Parse each type of configuration file
+    parseEmployeeConfiguration();
+    parseIngredientConfiguration();
+    parseMenuConfiguration();
   }
 
   /**
    * Parses the event file and passes on the information.
    */
   public void parseEvents() {
-    BufferedReader eventReader;
-    try {
-      eventReader = getConfigurationReader(EVENTS_FILE_NAME);
-    } catch (IOException e) {
-      // TODO: be more delicate
-      System.out.println("Finding configuration files failed.");
-      System.exit(1);
-      return;
-    }
+    // Make sure the configuration folder exists
+    verifyConfigurationFolder();
 
-    try {
-      parseEventsFile(eventReader);
-    } catch (IOException e) {
-      // TODO: be more delicate
-      System.out.println("Parsing of configuration files was not successful.");
-      System.exit(1);
-    }
+    // Parse the events file
+    parseEventsFile();
   }
 
   /**
@@ -110,16 +87,24 @@ public class Parser {
    *
    * @param fileName the name of the file to generate a reader for.
    * @return the reader generated.
-   * @throws IOException if something went horribly wrong in creating files.
    */
-  private BufferedReader getConfigurationReader(String fileName) throws IOException {
+  private BufferedReader getReader(String fileName) {
+    // Find the file we want to get the reader for
     File file = new File(filesLocation + fileName);
 
-    Boolean madeNew = file.createNewFile();
-    if (madeNew) {
-      System.out.println("Created a " + fileName + " configuration file.");
+    try {
+      // Attempt to create the file and notify if it was created now
+      if (file.createNewFile()) {
+        System.out.println(file.getName() + " not found. Created a new one.");
+      }
+
+      // Return the reader of the file whether or not we just created it
+      return new BufferedReader(new FileReader(file));
+    } catch (IOException e) {
+      System.out.println(file.getName() + " not found and could not be created.");
+
+      return null;
     }
-    return new BufferedReader(new FileReader(file));
   }
 
   /**
@@ -128,20 +113,30 @@ public class Parser {
    *
    * @param reader the reader to generate the symbols from.
    * @return the array of symbols representing the different parts of the line read.
-   * @throws IOException if something went horribly wrong in reading files.
    */
-  private String[] preParse(BufferedReader reader) throws IOException {
+  private String[] preParse(BufferedReader reader) {
     String[] symbols;
 
+    // Keep trying to pre-parse a line until we actually read a line with usable information
     do {
-      String line = reader.readLine();
+      // Try reading a line and notify if reading failed
+      String line = null;
+      try {
+        line = reader.readLine();
+      } catch (IOException e) {
+        System.out.println("Reading a line failed.");
+      }
+
+      // If we read a line then try to split the line into usable information
       if (line != null) {
+        // Make sure the line is not empty and is not a comment before trying to find information
         if (line.equals("") || line.charAt(0) == COMMENT_SYMBOL) {
           symbols = null;
         } else {
           symbols = line.split("\\s+");
         }
       } else {
+        // If the line is the end of the file return an empty array
         return new String[]{};
       }
     } while (symbols == null);
@@ -151,11 +146,10 @@ public class Parser {
 
   /**
    * Parses the employee configuration file. Reads in the names of employees.
-   *
-   * @param reader the reader of the employee configuration file.
-   * @throws IOException if something went horribly wrong in reading files.
    */
-  private void parseEmployeeConfiguration(BufferedReader reader) throws IOException {
+  private void parseEmployeeConfiguration() {
+    BufferedReader reader = getReader(EMPLOYEE_FILE_NAME);
+
     String[] symbols;
     while ((symbols = preParse(reader)) != null) {
       restaurant.employeeController.addEmployee(symbols[0] + symbols[1]);
@@ -164,11 +158,10 @@ public class Parser {
 
   /**
    * Parses the ingredient configuration file. Reads in the names of ingredients.
-   *
-   * @param reader the reader of the ingredient configuration file.
-   * @throws IOException if something went horribly wrong in reading files.
    */
-  private void parseIngredientConfiguration(BufferedReader reader) throws IOException {
+  private void parseIngredientConfiguration() {
+    BufferedReader reader = getReader(INGREDIENTS_FILE_NAME);
+
     String[] symbols;
     while ((symbols = preParse(reader)) != null) {
       restaurant.inventory.addIngredient(symbols[0], Integer.parseInt(symbols[1]), Integer.parseInt(symbols[2]));
@@ -178,11 +171,9 @@ public class Parser {
   /**
    * Parses the menu configuration file. Reads in the names of menus, their menu items, what they are made of and
    * what additions and subtractions can be added to each.
-   *
-   * @param reader the reader of the menu configuration file.
-   * @throws IOException if something went horribly wrong in reading files.
    */
-  private void parseMenuConfiguration(BufferedReader reader) throws IOException {
+  private void parseMenuConfiguration() {
+    BufferedReader reader = getReader(MENUS_FILE_NAME);
     String currentMenu = null;
 
     String[] symbols;
@@ -212,11 +203,10 @@ public class Parser {
 
   /**
    * Parses the events file. Offloads work to {@link EventParser}.
-   *
-   * @param reader the reader of the employee configuration file.
-   * @throws IOException if something went horribly wrong in reading files.
    */
-  private void parseEventsFile(BufferedReader reader) throws IOException {
+  private void parseEventsFile() {
+    BufferedReader reader = getReader(EVENTS_FILE_NAME);
+
     String[] symbols;
     while ((symbols = preParse(reader)) != null) {
       EventParser eventParser = new EventParser(restaurant);
