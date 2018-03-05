d0095a6aa2fee3557f2be72ebd6417facfefd967
Ziyad Edher <ziyad.edher@mail.utoronto.ca>
2018-03-05 14:03:24 -0500

    Implement MenuController

--- phase1/src/MenuController.java
+++ phase1/src/MenuController.java
@@ -1,2 +1,21 @@
+import java.util.HashMap;
+
 public class MenuController {
+    private HashMap<String, Menu> menus = new HashMap<>();
+
+    public void addMenu(String menuName) {
+        menus.put(menuName, new Menu());
+    }
+
+    public void addItemToMenu(String menuName, String menuItemName, int price) {
+        menus.get(menuName).addMenuItem(menuItemName, price);
+    }
+
+    public void addIngredientToMenuItem(String menuName, String menuItemName, String ingredientName) {
+        menus.get(menuName).addIngredientToMenuItem(menuItemName, ingredientName);
+    }
+
+    public void addAdditionToMenuItem(String menuName, String menuItemName, String additionName) {
+        menus.get(menuName).addAdditionToMenuItem(menuItemName, additionName);
+    }
 }
