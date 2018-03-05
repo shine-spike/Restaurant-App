951447deac218d6993ae56b8c3aa3bc5908c898a
Jack McKinney <jack.mckinney@mail.utoronto.ca>
2018-03-05 13:42:27 -0500

    Implement MenuItem, for the ultimate goal of implementing MenuItemController

--- phase1/src/MenuItem.java
+++ phase1/src/MenuItem.java
@@ -1,13 +1,50 @@
+import java.util.ArrayList;
+
 public class MenuItem {
+    int price;
+    String name;
+    ArrayList<Ingredient> ingredients;
+    ArrayList<Ingredient> substitutions;
+
+    MenuItem(int price, String name, ArrayList<Ingredient> ingredients, ArrayList<Ingredient> subs){
+        this.price = price;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.substitutions = subs;
+    }
+
     public int getPrice(){
-        return 0;
+        return price;
     }
 
     public String getName(){
-        return null;
+        return name;
+    }
+
+    public Ingredient[] getAllIngredients(){
+        ArrayList<Ingredient> allIngredients = new ArrayList<>();
+        allIngredients.addAll(ingredients);
+        ArrayList<Ingredient> newIngredients = new ArrayList<>();
+        for (Ingredient sub:
+             substitutions) {
+            if (allIngredients.contains(sub)){
+                Ingredient original = allIngredients.get(allIngredients.indexOf(sub));
+                original.setQuantity(original.getQuantity() + sub.getQuantity());
+            }
+            else {
+                newIngredients.add(sub);
+            }
+        }
+
+        allIngredients.addAll(newIngredients);
+        return allIngredients.toArray(new Ingredient[allIngredients.size()]);
     }
 
     public Ingredient[] getIngredients(){
-        return new Ingredient[0];
+        return ingredients.toArray(new Ingredient[ingredients.size()]);
+    }
+
+    public Ingredient[] getSubstitutions(){
+        return substitutions.toArray(new Ingredient[substitutions.size()]);
     }
 }
