430d5b233169b58afc8e65600d498d6bf273fba7
Ziyad Edher <ziyad.edher@gmail.com>
2018-03-05 15:48:03 -0500

    Add Javadocs

--- phase1/src/EventType.java
+++ phase1/src/EventType.java
@@ -1,3 +1,6 @@
+/**
+ * Stores all types of events that should be processed by the Parser and related information.
+ */
 public enum EventType {
   ORDER("ORDER", 3, false),
   SEEN("SEEN", 1, true),
@@ -18,12 +21,25 @@ public enum EventType {
   private boolean numberArgumentsFixed;
 
 
+  /**
+   * Constructs an EventType.
+   *
+   * @param keyword the word that triggers this event in the file.
+   * @param minimumArguments the minimum number of arguments that should be passed to this event.
+   * @param numberArgumentsFixed whether or not the minimum number of arguments is also the maximum.
+   */
   EventType(String keyword, int minimumArguments, boolean numberArgumentsFixed) {
     this.keyword = keyword;
     this.minimumArguments = minimumArguments;
     this.numberArgumentsFixed = numberArgumentsFixed;
   }
 
+  /**
+   * Finds the event type that corresponds to a given keyword.
+   *
+   * @param keyword the corresponding keyword.
+   * @return the event type corresponding to the given keyword or {@code null} if it does not exist.
+   */
   public static EventType getEventType(String keyword) {
     for(EventType e : EventType.values()){
       if(keyword.equals(e.keyword)) {
@@ -34,6 +50,12 @@ public enum EventType {
     return null;
   }
 
+  /**
+   * Checks if the given size of call is correct for this event type.
+   *
+   * @param size the number of symbols in a call, including the event itself and employee number.
+   * @return whether or not the size is compatible.
+   */
   public boolean correctSize(int size) {
     if (numberArgumentsFixed) {
       return size == this.minimumArguments + BASIC_ARGUMENT_NUMBER;
@@ -42,6 +64,11 @@ public enum EventType {
     }
   }
 
+  /**
+   * Returns a string representation of the number of arguments required for this event type.
+   *
+   * @return the string representation of the number of arguments required for this event type.
+   */
   public String getRequiredNumberArguments() {
     if (numberArgumentsFixed) {
       return "at least " + this.minimumArguments;
